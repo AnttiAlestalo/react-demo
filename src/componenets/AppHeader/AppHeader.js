85ee2cad83a59f1338bd5bbbaf559b2a5c3fb996
Antti Alestalo <antti.alestalo@gmail.com>
2018-10-22 13:20:35 +0300

    page layout change

--- src/componenets/AppHeader/AppHeader.js
+++ src/componenets/AppHeader/AppHeader.js
@@ -12,6 +12,14 @@ function AppHeader(props) {
     const { i18n } = props;
 
     return <React.Fragment>
+        <img src={logo} className="cssAppLogo" alt="logo" />
+        <div className="cssSiteTitle">{t('SiteTitle')}</div>
+
+        <div className="cssTopNav">
+            <NavLink exact to='/index.html' rel="noopener noreferrer" activeClassName="cssNav1A">{t('Home')}</NavLink>
+            <NavLink strict to='/customers' rel="noopener noreferrer" activeClassName="cssNav1A">{t('Customers')}</NavLink>
+            <NavLink exact to='/reports' rel="noopener noreferrer" activeClassName="cssNav1A">{t('Reports')}</NavLink>
+        </div>
         <Dropdown id="idDropdownSettings">
             <DropdownTrigger><FontAwesomeIcon icon={faCog} color="white" size="2x" title={t('Settings')} /></DropdownTrigger>
             <DropdownContent>
@@ -20,16 +28,6 @@ function AppHeader(props) {
                 <div className="cssSelection" onClick={() => i18n.changeLanguage('fi')}>Finnish</div>
             </DropdownContent>
         </Dropdown>
-        <div className="cssAppHeaderContainer">
-            <img src={logo} className="cssAppLogo" alt="logo" />
-            <div className="cssSiteTitle">{t('SiteTitle')}</div>
-
-            <div className="cssTopNav">
-                <NavLink exact to='/index.html' rel="noopener noreferrer" activeClassName="cssNav1A">{t('Home')}</NavLink>
-                <NavLink strict to='/customers' rel="noopener noreferrer" activeClassName="cssNav1A">{t('Customers')}</NavLink>
-                <NavLink exact to='/reports' rel="noopener noreferrer" activeClassName="cssNav1A">{t('Reports')}</NavLink>
-            </div>
-        </div>
     </React.Fragment>
 }
 
